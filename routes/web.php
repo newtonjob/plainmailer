00cd5ac0f92a7e3762f3715e1c9a9629802700ec
newtonjob <jobnewton3@gmail.com>
2025-04-22 12:24:20 +0100

    Update emails endpoint

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/up');
 
-Route::post('/send', function (SendMailRequest $request) {
+Route::post('/emails', function (SendMailRequest $request) {
     $request->mailer()->send(new AbstractMail($request->safe()));
 
     return response()->noContent();
