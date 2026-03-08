0dfd036cb5b2944ad8736b540a275284ccb629db
newtonjob <jobnewton3@gmail.com>
2026-03-08 08:48:19 +0100

    Improve mailer

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +9,5 @@ Route::redirect('/', '/up');
 Route::post('/emails', function (Request $request) {
     Mail::macro('forward', fn ($message) => $this->sendSymfonyMessage($message));
 
-    Mail::build($request->config)->forward(unserialize($request->message));
-});
+    Mail::build($request->config)->forward(unserialize(base64_decode($request->message)));
+})->withoutMiddleware(VerifyCsrfToken::class);
